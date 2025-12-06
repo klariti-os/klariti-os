31b0ca93977a7fdca5e7373ffad8c5d2100442af
ignasxv <62844231+ignasxv@users.noreply.github.com>
2025-12-06 11:19:59 -0500

    update: removed conditional auth page access

--- src/config/constants.ts
+++
@@ -1,6 +0,0 @@
-// src/config/constants.ts
-export const IS_PRODUCTION = process.env.NODE_ENV === 'production';
-
-export const ROUTES = {
-  AUTH_REDIRECT: IS_PRODUCTION ? '/join' : '/auth',   
-} as const;
