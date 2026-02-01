3ec13a1ebcdab235e9e026280fe8b7613c3bd014
ignasxv <62844231+ignasxv@users.noreply.github.com>
2026-02-01 17:44:59 -0500

    chore: Added fastify server

---
+++ apps/fastify/src/server.ts
@@ -0,0 +1,24 @@
+// ESM
+import Fastify from 'fastify'
+
+
+const fastify = Fastify({
+  logger: true
+})
+
+
+// Declare a route
+fastify.get('/', function (request, reply) {
+  reply.send({ "Message" : "hello from Klariti" })
+})
+
+
+// Run the server!
+fastify.listen({ port: 3000 }, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+  // Server is now listening on ${address}
+})
+
