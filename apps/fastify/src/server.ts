6f9b785f499d41cfd994dd60dc74eb296a857701
ignasxv <62844231+ignasxv@users.noreply.github.com>
2026-02-02 16:07:55 -0500

    chore: updated lockfiles

--- apps/fastify/src/server.ts
+++ apps/fastify/src/server.ts
@@ -1,17 +1,13 @@
-// ESM
 import Fastify from 'fastify'
-
 
 const fastify = Fastify({
   logger: true
 })
 
-
 // Declare a route
 fastify.get('/', function (request, reply) {
   reply.send({ "Message" : "hello from Klariti" })
 })
-
 
 // Run the server!
 fastify.listen({ port: 3000 }, function (err, address) {
