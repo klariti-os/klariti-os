d13051291c2d0bfcf890d8d75805a1a3af6f9a8f
ignasxv <62844231+ignasxv@users.noreply.github.com>
2026-02-05 01:43:48 -0500

    feat: reorganized the api to stay at the root

--- api/src/server.ts
+++ api/src/server.ts
@@ -9,7 +9,7 @@ const fastify = Fastify({
 });
 
 fastify.get("/favicon.ico", async (req, reply) => {
-  reply.redirect("https://agentic-house.vercel.app/favicons/favicon.ico")
+  reply.redirect("https://agentic-house.vercel.app/favicons/favicon.ico");
 });
 
 fastify.get("/", async (request, reply) => {
@@ -18,30 +18,28 @@ fastify.get("/", async (request, reply) => {
 
 fastify.post("/api/users", {
   handler: async (request: FastifyRequest, reply: FastifyReply) => {
-
     const body = request.body;
-    
-    console.log( {body} )
 
-    return reply.code(201).send(body)
-  }, 
+    console.log({ body });
+
+    return reply.code(201).send(body);
+  },
 });
 
 async function main() {
   try {
-    await fastify.listen({ port: 5001, host: "0.0.0.0" });
+    await fastify.listen({ port: 4200, host: "0.0.0.0" });  
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
   }
 }
 
-["SIGINT", "SIGTERM"].forEach( (signal) => {
+["SIGINT", "SIGTERM"].forEach((signal) => {
   process.on(signal, async () => {
     await fastify.close();
     process.exit(0);
-  } )
-} )
-
+  });
+});
 
 main();
