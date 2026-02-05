4bc00ce9c3753ca88fafdc529ab6438ea21106db
ignasxv <62844231+ignasxv@users.noreply.github.com>
2026-02-05 01:16:18 -0500

    --amend

--- apps/fastify/src/server.ts
+++ apps/fastify/src/server.ts
@@ -1,4 +1,4 @@
-import Fastify from "fastify";
+import Fastify, { FastifyReply, FastifyRequest } from "fastify";
 
 const fastify = Fastify({
   logger: {
@@ -8,17 +8,34 @@ const fastify = Fastify({
   },
 });
 
+fastify.get("/favicon.ico", async (req, reply) => {
+  reply.redirect("https://agentic-house.vercel.app/favicons/favicon.ico")
+});
+
 fastify.get("/", async (request, reply) => {
   reply.send({ message: "hello from Klariti" });
+});
+
+fastify.post("/api/users", {
+  handler: async (request: FastifyRequest, reply: FastifyReply) => {
+
+    const body = request.body;
+    
+    console.log( {body} )
+
+    return reply.code(201).send(body)
+  }, 
 });
 
 async function main() {
   try {
     await fastify.listen({ port: 5001, host: "0.0.0.0" });
   } catch (err) {
-    fastify.log.error(err); 
-    process.exit(1); 
+    fastify.log.error(err);
+    process.exit(1);
   }
 }
+
+
 
 main();
