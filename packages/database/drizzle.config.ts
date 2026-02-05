af580ea428e61b482b96defb7f256f8d63f65ea9
ignasxv <62844231+ignasxv@users.noreply.github.com>
2026-02-05 12:34:44 -0500

    fix: type-check db url at compile tme

--- packages/database/drizzle.config.ts
+++ packages/database/drizzle.config.ts
@@ -1,13 +1,11 @@
 import { config } from "dotenv";
 import { defineConfig } from "drizzle-kit";
 
-config({ path: "../../.env" });
-
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL as string,
   },
 });
