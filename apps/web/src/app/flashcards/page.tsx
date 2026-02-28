cb67b8219f00bfd7d5ab44a1174083591abf6c43
ignasxv <62844231+ignasxv@users.noreply.github.com>
2026-02-28 09:40:25 -0500

    chore: remove old flashcards

--- apps/web/src/app/flashcards/page.tsx
+++
@@ -1,9 +0,0 @@
-import FlashcardDemo from "@/components/flashcards/FlashcardDemo";
-
-export default function FlashcardsPage() {
-  return (
-    <main className="mx-auto max-w-4xl p-6 pt-10">
-      <FlashcardDemo />
-    </main>
-  );
-}
