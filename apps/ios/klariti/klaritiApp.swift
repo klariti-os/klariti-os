3ae7c827537436df0a1f41aec1199478eba7a565
ignasxv <62844231+ignasxv@users.noreply.github.com>
2026-03-04 15:36:56 -0500

    chore: organizing code into components

--- apps/ios/klariti/klaritiApp.swift
+++ apps/ios/klariti/klaritiApp.swift
@@ -2,31 +2,17 @@
 //  klaritiApp.swift
 //  klariti
 //
-//  Created by ignas Kamugisha on 3/4/26.
-//
 
 import SwiftUI
-import SwiftData
 
 @main
 struct klaritiApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
+    private let store = AppStore()
 
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(store)
         }
-        .modelContainer(sharedModelContainer)
     }
 }
