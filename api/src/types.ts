afa88b2e6df6a7bacb44777f4f449752451809e9
ignasxv <62844231+ignasxv@users.noreply.github.com>
2026-04-05 13:29:10 -0400

    fix: restore @klariti/api/contracts export for web and extension consumers
    Added api/src/types.ts as a barrel that re-exports all public types from
    domain module schemas. Updated package.json ./contracts export to point
    to this file, fixing the broken Vercel build caused by the deleted
    src/contracts/index.ts.

---
+++ api/src/types.ts
@@ -0,0 +1,7 @@
+export type * from "./modules/auth/schemas.js";
+export type * from "./modules/me/schemas.js";
+export type * from "./modules/challenges/schemas.js";
+export type * from "./modules/friends/schemas.js";
+export type * from "./modules/ktags/schemas.js";
+export type * from "./modules/classify/schemas.js";
+export type * from "./modules/common/errors.schemas.js";
